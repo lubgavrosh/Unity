7ddc4dce44793efa4be4fe97e1b053bb08846e3f
Liubomyr Havrosh <lubgavrosh@gmail.com>
2023-12-02 12:08:39 +0100

    add sun

---
+++ Castle/Assets/Scripts/SunScript.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class SunScript : MonoBehaviour
+{
+    public float dayDuration = 15f;
+    public Color dayColor = new Color(0.5f, 0.5f, 1f);
+    public Color nightColor = new Color(0f, 0f, 0.2f);
+    public Vector3 startDayPosition = new Vector3(-82.5f, -3.27f, 150f);
+    public Vector3 halfDayPosition = new Vector3(60.1f, 3f, 150f);
+    public Vector3 endDayPosition = new Vector3(200.8f, -16f, 150f);
+    public float startDayHeight = -3.27f;
+    public float endDayHeight = -13f;
+    public float maxDayHeight = 3f;
+
+    private bool isDay = true;
+    private float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (isDay)
+        {
+            if (timer < dayDuration / 2f)
+            {
+                MoveSun(startDayPosition, halfDayPosition, startDayHeight, maxDayHeight, true);
+            }
+            else
+            {
+                MoveSun(halfDayPosition, endDayPosition, maxDayHeight, endDayHeight, false);
+            }
+
+            if (timer >= dayDuration)
+            {
+                timer = 0f;
+                isDay = false;
+                ChangeBackgroundColor(nightColor);
+            }
+        }
+        else
+        {
+            MoveSun(endDayPosition, endDayPosition, endDayHeight, endDayHeight, false);
+
+            if (timer >= dayDuration)
+            {
+                timer = 0f;
+                isDay = true;
+                ChangeBackgroundColor(dayColor);
+            }
+        }
+    }
+
+ void MoveSun(Vector3 from, Vector3 to, float startHeight, float endHeight, bool raising)
+{
+    float t = timer / (dayDuration / 2f);
+
+    if (!raising)
+        t = 1 - t;
+
+    float t1 = 3f * t * t - 2f * t * t * t;
+
+        float height = Mathf.Lerp(startHeight, endHeight, t);
+        transform.position = Vector3.Lerp(from, to, t1);
+    transform.position = new Vector3(transform.position.x, height, transform.position.z);
+}
+
+    void ChangeBackgroundColor(Color targetColor)
+    {
+        Camera.main.backgroundColor = targetColor;
+    }
+}
