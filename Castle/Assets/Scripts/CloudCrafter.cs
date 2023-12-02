833c092667ff31b8adda1ad9801239e2a62e3b3b
Liubomyr Havrosh <lubgavrosh@gmail.com>
2023-12-02 08:41:58 +0100

    moving clouds

--- Castle/Assets/Scripts/CloudCrafter.cs
+++ Castle/Assets/Scripts/CloudCrafter.cs
@@ -3,13 +3,13 @@ using System.Collections;
 public class CloudCrafter : MonoBehaviour
 {
     [Header("Set in Inspector")]
-    public int numClouds = 40;
+    public int numClouds = 20;
     public GameObject cloudPrefab;
-    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3 cloudPosMin = new Vector3(-50, 40, 10);
     public Vector3 cloudPosMax = new Vector3(150, 100, 10);
     public float cloudScaleMin = 1;
     public float cloudScaleMax = 3;
-    public float cloudSpeedMult = 0.5f;
+    public float cloudSpeedMult = 2f;
 
     private GameObject[] cloudInstances;
 
@@ -42,6 +42,7 @@ public class CloudCrafter : MonoBehaviour
             cloud.transform.localScale = Vector3.one * scaleVal;
             // Сделать облако дочерним по отношению к anchor
             cloud.transform.SetParent(anchor.transform);
+            cloudInstances[i] = cloud;
         }
     }
         void Update()
